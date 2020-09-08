c9286779f698ccf1c607b9db7fba09ea6d88b146
Adanieljohnson91 <anthonydanjohnson@gmail.com>
2020-09-08 08:30:08 -0500

    Full StreamExample

---
+++ Class.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StreamReader
+{
+    public class Reader
+    {
+        public string[] test = {"This", "Is", "A", "Test" };
+
+        public void Write()
+        {
+            using (System.IO.StreamWriter writer = new System.IO.StreamWriter(@"C:\Users\DanJo\source\repos\StreamReader\StreamReader\TextFile.txt"))
+            {
+                foreach(string word in test)
+                {
+                    writer.WriteLine(word);
+                }
+            }
+        }
+        
+    }
+}
